41c0c601b08d2b4eb729caecbbb6cadb76fd5c7a
cuinixam <22344617+cuinixam@users.noreply.github.com>
2025-03-08 15:16:48 +0100

    fix: add cname to gh-pages deployment

--- docs/conf.py
+++ docs/conf.py
@@ -32,12 +32,7 @@ extensions.append("ablog")
 # https://myst-parser.readthedocs.io/en/latest/intro.html
 extensions.append("myst_parser")
 
-myst_enable_extensions = [
-    "colon_fence",
-    "deflist",
-    "html_admonition",
-    "html_image",
-]
+myst_enable_extensions = ["colon_fence", "deflist", "html_admonition", "html_image", "attrs_inline"]
 
 # mermaid config - @see https://pypi.org/project/sphinxcontrib-mermaid/
 extensions.append("sphinxcontrib.mermaid")
